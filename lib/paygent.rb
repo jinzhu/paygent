39a86ed334c9eb6ba2c1b08fcc499c5639764957
Jinzhu <wosmvp@gmail.com>
2012-04-04 18:39:37 +0800

    update paygent

--- lib/paygent.rb
+++ lib/paygent.rb
@@ -1,7 +1,20 @@
 require "paygent/version"
 
 module Paygent
-  attr_accessor :client_file_path, :ca_file_path, :log_output_path, :debug_flg, :default_id, :default_password
+  attr_accessor :client_file_path, :ca_file_path, :log_output_path, :debug_flg, :default_id, :default_password, :timeout, :select_max_cnt
+
+  Paygent.client_file_path = ""
+  Paygent.ca_file_path     = ""
+  Paygent.log_output_path  = File.join(Rails.root, 'log/paygent.log')
+  Paygent.debug_flg        = 0
+  Paygent.default_id       = ""
+  Paygent.default_password = ""
+  Paygent.timeout          = 0
+  Paygent.select_max_cnt   = 0
+
+  def self.init
+    # Init Logger
+  end
 end
 
 # w/ 3D code
