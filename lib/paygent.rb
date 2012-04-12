95e272b3a9ade3fd5c207bab1259d0e9e7f00348
Jinzhu <wosmvp@gmail.com>
2012-04-12 11:43:48 +0800

    Add merchant_id, telegram_version

--- lib/paygent.rb
+++ lib/paygent.rb
@@ -1,6 +1,7 @@
 module Paygent
   class << self
-    attr_accessor :client_file_path, :ca_file_path, :log_output_path, :debug_flg, :default_id, :default_password, :timeout, :select_max_cnt, :cert_password
+    attr_accessor :client_file_path, :ca_file_path, :log_output_path, :debug_flg, :timeout, :select_max_cnt, :cert_password
+    attr_accessor :merchant_id, :default_id, :default_password, :telegram_version
   end
 
   Paygent.client_file_path = "config/paygent/client_mdev_20090123.pem"
